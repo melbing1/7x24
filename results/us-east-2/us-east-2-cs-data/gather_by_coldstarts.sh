0feb36d00634e4cac2231ccd2592ed6013cd9013
Matthew Elbing <melbing1@pride.hofstra.edu>
2021-02-18 16:15:40 -0500

    add us-east-2 cold start data and graph. Also add script to parse timeout periods for graphing

---
+++ results/us-east-2/us-east-2-cs-data/gather_by_coldstarts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# USAGE: ./gather_by_coldstarts.sh
+# This will create a result file for each memory + coldstart combination with each of the timeouts
+
+declare -a mem_sizes=("128" "256" "512" "1024" "1152" "1280" "1408" "1536" "1792" "1856" "2048" "2240" "2432" "2624" "2816" "3008")
+
+for cs in 1 2 3 4 5 6 7 8 9
+do
+	for sz in "${mem_sizes[@]}"
+	do
+		cat us-east-2.json | grep -A 3 -B 3 "\"coldstarts\": \"$cs\"" | grep -A 3 -B 3 "\"mem\": $sz" | grep "interval" | xargs echo > coldstarts_${sz}_${cs}
+	done
+done
