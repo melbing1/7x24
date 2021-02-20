064407ebcda33e04fbf65759b29df10568097ad8
Matthew Elbing <melbing1@pride.hofstra.edu>
2021-02-20 13:29:18 -0500

    Add us west data

---
+++ results/us-west-1/gather_by_coldstarts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# USAGE: ./gather_by_coldstarts.sh
+# This will create a result file for each memory + coldstart combination with each of the timeouts
+
+declare -a mem_sizes=("128" "256" "512" "1024" "1152" "1280" "1408" "1536" "1792" "1856" "2048" "2240" "2432" "2624" "2816" "3008")
+
+for cs in 1 2 3 4 5 6 7 8 9
+do
+	for sz in "${mem_sizes[@]}"
+	do
+		cat $1 | grep -A 3 -B 3 "\"coldstarts\": \"$cs\"" | grep -A 3 -B 3 "\"mem\": $sz" #| grep "interval"
+	done
+	echo ""
+done
