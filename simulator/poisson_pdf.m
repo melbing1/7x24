a6e8a6fe3312a7f3b79429ad6834a4bc5dd8003a
Matthew Elbing <melbing1@pride.hofstra.edu>
2021-03-25 20:02:06 -0400

    move files

--- simulator/poisson_pdf.m
+++
@@ -1,9 +0,0 @@
-range = 0:30; % Requests arrive between Minutes
-ProbDensityFn = poisspdf(range, 15); % λ = 8 (centers the distrobution)
-
-figure
-bar(range, ProbDensityFn, 1); % Bar width = 1
-xlabel('Time between Requests (s)');
-ylabel('Probability');
-title('Request Distrobution');
-
